b7f14291f457e6c45f89d0fd9ac940c846b9c6ee
Ashraful Hossen Thusar <ashraful35-1908@diu.edu.bd>
2020-04-09 16:55:58 +0600

    first commit

---
+++ course1.php
@@ -0,0 +1,17 @@
+<?php
+
+
+$conn =  mysqli_connect("remotemysql.com", "R4t3eLOhus", "7PBlaT2HqV" , "R4t3eLOhus");
+// Check connection
+// Check connection
+if (!$conn) {
+    die("Connection failed: ". mysqli_connect_error());
+}
+ $file = 'c.pdf';
+ $filename = 'c.pdf';
+ header('Content-type:application.pdf');
+ header('Content-Disposition: inline; filename"' . $filename .'"');
+ header('Content-Transfer-Encoding: binary');
+ header('Accept-Ranges: bytes');
+ @readfile($file);
+?>
